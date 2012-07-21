fb7008917eaef63ff91903b8683cd7585f318811
Andrew Pinzler <pinzler@gmail.com>
2012-07-21 14:48:19 -0400

    update tables

--- components/add-tells.php
+++ components/add-tells.php
@@ -1,17 +1,18 @@
-<form class="well">
+<form class="well" name="tellsform" action="http://fourtell.co/components/updateTable.php" method="post">
   <label>Add a new friend</label>
-  <input type="text" class="span3" placeholder="Friend's name…">
+  <input type="text" name="friend" class="span3" placeholder="Friend's name…">
   <label>Enter the email or cell phone # to use. Just use numbers</label>
-  <input type="text" class="span3" placeholder="example@example.com or 6666666666">
+  <input type="text" name="contact" class="span3" placeholder="example@example.com or 6666666666">
   <label class="radio">
-    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
+    <input type="radio" name="optionsRadios" value="email" checked>
     This is an email address
   </label>
   <label class="radio">
-    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+    <input type="radio" name="optionsRadios" value="phone">
     This is a cell phone number
   </label>
   <label>Enter the Tell groups for this friend. Must be entered as #hashtags separated by commas</label>
-  <input type="text" class="span3" placeholder="#example, #example2…"><br>
-  <button type="submit" class="btn btn-success">Submit</button>
+  <input type="text" name="tells" class="span3" placeholder="#example, #example2…"><br>
+  <input type="hidden" name="adddel" value="add">
+  <input type="submit" class="btn btn-success" value="Submit">
 </form>
