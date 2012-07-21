420e9b226fd783feb3af2f493d19f193adc4ebc2
Dave Gustav <dave@davidgustav.com>
2012-07-21 19:55:21 -0400

    kjhd

--- components/add-tells.php
+++ components/add-tells.php
@@ -14,11 +14,11 @@
 			<form id="tells-form" class="form-horizontal" name="tellsform" action="components/updateTable.php" method="get">
 				<div class="control-group">
 					<label>Friend's name. Warning: This is how they will be addressed in emails. Only use names you'd say to their face.</label>
-					<input type="text" name="friend" id="friend" class="span3" placeholder="Friend's name…">
+					<input type="text" name="friend" id="friend" class="span4" placeholder="Friend's name…">
 				</div>
 				<div class="control-group">
-					<label>Enter the email or cell phone # (If using cell, numbers only. No dashes or parentheses) to use. If you send text messages, they will come from (216) 503-TELL.</label>
-					<input type="text" name="contact" id="contact" placeholder="example@example.com or 6666666666">
+					<label>Enter the email or cell phone # to use (If using cell, numbers only. No dashes or parentheses.) If you send text messages, they will come from (216) 503-TELL.</label>
+					<input type="text" name="contact" id="contact" class="span4" placeholder="example@example.com or 6666666666">
 				</div>
 				<div class="control-group">
 					<label class="radio">
@@ -32,7 +32,7 @@
 				</div>
 				<div class="control-group">
 					<label>Enter the <strong>"Tell"</strong> groups for this friend. Must be entered as #hashtags separated by commas</label>
-					<input type="text" name="tells" id="tells" class="span3" placeholder="#example, #example2…">
+					<input type="text" name="tells" id="tells" class="span4" placeholder="#example, #example2…">
 				</div>
 				<input type="hidden" name="adddel" value="add">
 				<input type="submit" class="btn btn-success" value="Submit">
--- header.php
+++ header.php
@@ -8,8 +8,7 @@
     <meta charset="utf-8">
     <title>FourTell: Check In and Tell Your Luddite Friends</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Let's face it – not many of your friends use foursquare and sharing is the whole point! FourTell lets you create groups of friends by interests and notifies them when you use that hashtag in a foursquare check in.">
 
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
     <style>      
@@ -48,12 +47,7 @@
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
-    <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="../assets/ico/favicon.ico">
-    <!--<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">-->
   </head>
 
   <body>
