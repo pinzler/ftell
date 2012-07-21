4cde4623f3fec7ee5608534f522f8f37f40dac9d
Dave Gustav <dave@davidgustav.com>
2012-07-21 13:40:14 -0400

    added include file for adds

---
+++ components/add-tells.php
@@ -0,0 +1,17 @@
+<form class="well">
+  <label>Add a new friend</label>
+  <input type="text" class="span3" placeholder="Friend's name…">
+  <label>Enter the email or cell phone # to use. Just use numbers</label>
+  <input type="text" class="span3" placeholder="example@example.com or 6666666666">
+  <label class="radio">
+    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
+    This is an email address
+  </label>
+  <label class="radio">
+    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+    This is a cell phone number
+  </label>
+  <label>Enter the Tell groups for this friend. Must be entered as #hashtags separated by commas</label>
+  <input type="text" class="span3" placeholder="#example, #example2…">
+  <button type="submit" class="btn">Submit</button>
+</form>
