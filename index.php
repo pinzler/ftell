9394f4d47c6a7be28a6c0408864b13a000f598ba
Dave Gustav <dave@davidgustav.com>
2012-07-21 13:42:22 -0400

    changed url in login button and added temp test for form fields

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php 
 include "header.php";
 ?>
-<a href="https://foursquare.com/oauth2/authenticate?client_id=J0LRFXR1NYQYYWHGEDJJ0VIMMV101D422Q5ISF5L13RNQFQE&response_type=code&redirect_uri=http://www.lampsquare.com/4sq.php"><img alt="Foursquare" src="https://playfoursquare.s3.amazonaws.com/press/logo/connect-blue.png"></a>
+
+<div class="hero-unit">
+  <h1>FourTell</h1>
+  <h2>Bringing the check-in party to your luddite friends.</h2>
+  <p>Get started now by connecting to your foursquare account</p>
+  <p>
+    <a class="btn" href="https://foursquare.com/oauth2/authenticate?client_id=J0LRFXR1NYQYYWHGEDJJ0VIMMV101D422Q5ISF5L13RNQFQE&response_type=code&redirect_uri=http://fourtell.co/login.php"><img alt="Foursquare" src="https://playfoursquare.s3.amazonaws.com/press/logo/connect-blue.png"></a>
+  </p>
+</div>
 
 <?php
+include "components/add-tells.php";
 include "footer.php";
 ?>
