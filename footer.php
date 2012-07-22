649c67cb2e92a29862e121afb025fdc1320368c4
Dave Gustav <dave@davidgustav.com>
2012-07-21 20:02:31 -0400

    added links

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 	<p>Built by <a href="http://twitter.com/pinzler">@pinzler</a> and  <a href="http://twitter.com/davidgustav">@davidgustav</a> for <a href="https://www.hackerleague.org/hackathons/battle-of-the-braces-round-2-php/">Battle of the Braces Round 2: PHP</a>
 </p>
 
-<p>Built using <a href="http://twitter.com/twilio">Twilio</a>,  <a href="http://twitter.com/sendgrid">SendGrid</a>, <a href="http://twitter.com/twitterapi">Twitter</a>, and <a href="http://twitter.com/twbootstrap">Twitter Bootstrap</a>
+<p>Built using <a href="http://twitter.com/foursquare">foursquare</a>, <a href="http://twitter.com/twilio">Twilio</a>,  <a href="http://twitter.com/sendgrid">SendGrid</a>, <a href="http://twitter.com/twitterapi">Twitter</a>, <a href="http://twitter.com/bitly">bit.ly</a>, and <a href="http://twitter.com/twbootstrap">Twitter Bootstrap</a>
 </p>
 </div>
 
